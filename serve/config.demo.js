1b7cfdd0c9670d04085f2ffec3bcd0853afa1387
Jax Chu <294242589@qq.com>
2019-03-31 23:49:45 +0800

    config.demo.js

---
+++ serve/config.demo.js
@@ -0,0 +1,15 @@
+const configs = {
+  mysql: {
+    host: '127.0.0.1',
+    port: '3306',
+    user: 'root',
+    password: '123456',
+    database: 'combat-okr'
+  },
+  miniapp: {
+    appid: 'xxx',
+    secret: 'xxx',
+  },
+}
+
+module.exports = configs
